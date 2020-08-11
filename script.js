fc09a302a6ee5bd7a3a29a71366719e130e30a28
Alexander Ose <alexander@parsely.com>
2020-08-11 12:51:29 -0700

    Experiments with bin packing

--- script.js
+++ script.js
@@ -213,8 +213,22 @@ function display(data) {
        
         setTimeout(() => {
             image.style.opacity = 1;
-        }, 10);
+            fitImages();
+        }, 1000);
     }
+}
+
+function fitImages() {
+    const images = document.querySelectorAll('.thumb');
+    const blocks = Array.from(images).map(d => ({ w: d.offsetWidth, h: d.offsetHeight, el: d }));
+    const result = pckr.fit(blocks);
+    blocks.forEach(({ el, fit }) => {
+        el.style.position = 'absolute';
+        el.style.left = fit.x;
+        el.style.top = fit.y;
+    });
+
+
 }
 
 // Append image to DOM in order to allow preload
@@ -415,4 +429,46 @@ function md5(inputString) {
     }
     return rh(a)+rh(b)+rh(c)+rh(d);
 }
+
+
+
+const Packer = function(w, h) {
+  this.init(w, h);
+};
+
+Packer.prototype = {
+
+  init: function(w, h) {
+    this.root = { x: 0, y: 0, w: w, h: h };
+  },
+
+  fit: function(blocks) {
+    var n, node, block;
+    for (n = 0; n < blocks.length; n++) {
+      block = blocks[n];
+      if (node = this.findNode(this.root, block.w, block.h))
+        block.fit = this.splitNode(node, block.w, block.h);
+    }
+  },
+
+  findNode: function(root, w, h) {
+    if (root.used)
+      return this.findNode(root.right, w, h) || this.findNode(root.down, w, h);
+    else if ((w <= root.w) && (h <= root.h))
+      return root;
+    else
+      return null;
+  },
+
+  splitNode: function(node, w, h) {
+    node.used = true;
+    node.down  = { x: node.x,     y: node.y + h, w: node.w,     h: node.h - h };
+    node.right = { x: node.x + w, y: node.y,     w: node.w - w, h: h          };
+    return node;
+  }
+
+}
+
+const pckr = new Packer(4000, 800);
+
 })();
